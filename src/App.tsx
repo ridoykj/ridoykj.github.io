6140e42ad6db8adbd914b5f824c5c6d5bf01e6e9
ridoy.kumar <ridoy.kumar@gmail.com>
2024-05-07 20:43:59 +0600

    chore: Update App.tsx to remove unused imports and update component structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
 import './App.css'
+import HeaderRC from './components/HeaderRC'
+import IntroRC from './components/IntroRC'
+import PortfolioRC from './components/PortfolioRC'
+import ServiceRC from './components/ServiceRC'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='bg-gradient-to-tr from-white from-70% to-purple-200 h-full '>
+        <div className="mx-auto max-w-screen-xl">
+          <HeaderRC />
+          <IntroRC />
+          <ServiceRC />
+          <PortfolioRC />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
